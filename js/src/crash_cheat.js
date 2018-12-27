92d1b2bc3f7388199e7a84a8bb96e6e5f9f2fd6b
honeyhhhh <1842212797@qq.com>
2018-12-27 14:11:36 +0800

    Site updated: 2018-12-27 14:11:35

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -3,12 +3,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/img/TEP.ico");
+        $('[rel="icon"]').attr('href', "/public/images.favicon-16x16-next.png");
         document.title = '页面找不到了';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
+        $('[rel="icon"]').attr('href', "/public/images.favicon-16x16-next.png");
         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
